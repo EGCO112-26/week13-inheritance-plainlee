ed2ee6328fc3daaccdbc15717674a98898aa43be
plainlee <plain.lee@student.mahidol.edu>
2026-04-01 09:54:50 +0000

    Fixed 1st attempt

--- main.cpp
+++ main.cpp
@@ -28,6 +28,45 @@
 // // Add constructor --> set name 
 // // modify display() -->id/name/gpa
 
-//ไม่ใช้แล้ว main หลักอยู่ที่ LL_test.cpp
+//เก็บไว้ เผื่อพัง
 
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+
+#include "NODE.h"
+#include "Thai_person.h"
+#include "MU_Person.h"
+#include "student.h"
+#include "LL.h"
+
+
+#include "NODE.cpp"
+#include "LL.cpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    LL myLL;
+
+    for (int i = 1; i < argc; i += 3) {
+        if (i + 2 < argc) {
+            long id = atol(argv[i]);
+            double gpa = atof(argv[i+1]);
+            string name = argv[i+2];
+            
+            NODE* A = new student(id, gpa, name, 1234); 
+            myLL.add_node(A);
+        }
+    }
+
+    cout << endl;
+    
+    myLL.show_all();
+    
+    cout << endl;
+
+    return 0;
+}
 
